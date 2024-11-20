635483f38d29086732947442c5a9c79cae4628b1
Aditya Tile <adityatile99@gmail.com>
2024-11-20 20:23:43 +0530

    create left section of product page

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
-function LeftSection() {
+function LeftSection({
+  imageUrl,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
   return (
-    <div>LeftSection</div>
-  )
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-6">
+          <img src={imageUrl} />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={tryDemo} className="text-decoration-none">
+              Try demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+            <a
+              href={learnMore}
+              style={{ marginLeft: "50px" }}
+              className="text-decoration-none"
+            >
+              Learn more{" "}
+              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img src="media/images/googlePlayBadge.svg" />
+            </a>
+            <a href={appStore}>
+              <img
+                src="media/images/appStoreBadge.svg"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LeftSection
+export default LeftSection;
