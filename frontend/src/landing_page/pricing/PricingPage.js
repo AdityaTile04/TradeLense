913cd83b6625885f3e79d464a46a9d86a8c97fb6
Aditya Tile <adityatile99@gmail.com>
2025-01-31 23:14:51 +0530

    complete hero section of pricing page

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
+import Brokerage from "./Brokerage";
 
 function PricingPage() {
   return (
-    <div>PricingPage</div>
-  )
+    <>
+      <Hero />
+      <OpenAccount />
+      <Brokerage />
+    </>
+  );
 }
 
-export default PricingPage
+export default PricingPage;
