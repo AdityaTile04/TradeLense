6686fa810d166985bed9d2bb0ec23d5f0509908a
Aditya Tile <adityatile99@gmail.com>
2025-02-02 23:16:35 +0530

    create position schema and model

---
+++ backend/model/position.model.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const positionsSchema = require("../schemas/positionSchema");
+
+const Position = mongoose.model("Position", positionsSchema);
+
+module.exports = Position;
