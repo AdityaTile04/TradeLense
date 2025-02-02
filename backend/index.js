fe3a88bd14e452f37e85f2d325a59b5f8ca552f8
Aditya Tile <adityatile99@gmail.com>
2025-02-02 17:48:11 +0530

    create database setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running port ${PORT}`);
+});
+
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+};
+connection()
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(`Error while connecting database ${err}`));
