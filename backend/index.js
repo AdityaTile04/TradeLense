6f2c7dacc682d9d58f5bb976741b482f27b1c9d5
Aditya Tile <adityatile99@gmail.com>
2025-02-08 12:54:19 +0530

    create login route succesfully

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const Order = require("./model/order.model");
 const User = require("./model/user.model");
 const { createSecretToken } = require("./utils/SecretToken");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -56,6 +57,36 @@ app.post("/signup", async (req, res, next) => {
     res
       .status(201)
       .json({ message: "User signed in successfully", success: true, user });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "All fields are required!" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.json({ message: "Incorrect email or password" });
+    }
+
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      res.json({ message: "Incorrect password or email" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+    next();
   } catch (err) {
     console.log(err);
   }
